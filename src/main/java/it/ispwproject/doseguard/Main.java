51f1aaa8ffa53102b46a2a1cdad4fdbcdb7b4957
Evangelisti-Francesco <FRANCESCO.EVANGELISTI@STUDENTS.UNIROMA2.EU>
2026-08-18 11:48:36 +0200

    Correzioni smell

--- src/main/java/it/ispwproject/doseguard/Main.java
+++ src/main/java/it/ispwproject/doseguard/Main.java
@@ -10,5 +10,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Printer.println("Welcome to the DOSE Guard!");
+        Scanner sc = new Scanner(System.in);
+
     }
 }
