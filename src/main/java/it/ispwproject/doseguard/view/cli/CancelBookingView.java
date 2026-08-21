9455ec938cc3034b294dcb3b8d4c85f2ac5417b7
Evangelisti-Francesco <FRANCESCO.EVANGELISTI@STUDENTS.UNIROMA2.EU>
2026-08-21 15:36:35 +0200

    Aggiornamento build.yml

--- src/main/java/it/ispwproject/doseguard/view/cli/CancelBookingView.java
+++ src/main/java/it/ispwproject/doseguard/view/cli/CancelBookingView.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 public class CancelBookingView {
 
+    private static final String DOCTOR_PREFIX = "Dott. ";
+
     public void mostraIntestazione() {
         CLIRenderer.intestazione("DoseGuard  –  Annulla un appuntamento");
     }
@@ -18,7 +20,7 @@ public class CancelBookingView {
                 .mapToInt(b -> b.getSpecialization().getName().length())
                 .max().orElse(15);
         int doctorW = cancellable.stream()
-                .mapToInt(b -> ("Dott. " + b.getDoctor().getName() + " " + b.getDoctor().getSurname()).length())
+                .mapToInt(b -> ("DOCTOR_PREFIX. " + b.getDoctor().getName() + " " + b.getDoctor().getSurname()).length())
                 .max().orElse(18);
         int numW = String.valueOf(cancellable.size()).length();
 
@@ -29,7 +31,7 @@ public class CancelBookingView {
             System.out.printf(fmt,
                     i + 1,
                     b.getSpecialization().getName(),
-                    "Dott. " + b.getDoctor().getName() + " " + b.getDoctor().getSurname(),
+                    DOCTOR_PREFIX + b.getDoctor().getName() + " " + b.getDoctor().getSurname(),
                     b.getSlot().getDate(),
                     b.getSlot().getStartTime());
         }
@@ -39,7 +41,7 @@ public class CancelBookingView {
     public void mostraRiepilogo(AppointmentResponseBean selected) {
         CLIRenderer.sezione("Conferma annullamento");
         CLIRenderer.campo("Specializzazione", selected.getSpecialization().getName());
-        CLIRenderer.campo("Medico",           "Dott. " + selected.getDoctor().getName() + " " + selected.getDoctor().getSurname());
+        CLIRenderer.campo("Medico",           DOCTOR_PREFIX + selected.getDoctor().getName() + " " + selected.getDoctor().getSurname());
         CLIRenderer.campo("Data",             selected.getSlot().getDate() + "  ore " + selected.getSlot().getStartTime());
     }
 
